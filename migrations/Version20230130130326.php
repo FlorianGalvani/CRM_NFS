ccddb37fbef158d4f863deb308e33121a343682b
Florian Galvani <florian.galvani@gmail.com>
2023-01-30 19:40:00 +0100

    Add Quotes form partially

--- migrations/Version20230130130326.php
+++ migrations/Version20230130130326.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230130093327 extends AbstractMigration
+final class Version20230130130326 extends AbstractMigration
 {
     public function getDescription(): string
     {

96390684f588124c3d791d91f57bb0212d867314
QVan27 <qvannarath@gmail.com>
2023-02-02 15:56:32 +0100

    🚧 [GLOBAL] - Removing unused components

--- assets/js/layouts/tables/index.js
+++ assets/js/layouts/tables/index.js
@@ -51,13 +51,43 @@ function Tables() {
   useEffect(() => {
     decodedToken();
   }, []);
-  
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <MDBox pt={6} pb={3}>
         <Grid container spacing={6}>
+          {
+            token?.account === "admin" ? (
+              null
+            ) : <Grid item xs={12}>
+              <Card>
+                <MDBox
+                  mx={2}
+                  mt={-3}
+                  py={3}
+                  px={2}
+                  variant="gradient"
+                  bgColor="info"
+                  borderRadius="lg"
+                  coloredShadow="info"
+                >
+                  <MDTypography variant="h6" color="white">
+                    {token?.account === "admin" ? "Commerciaux" : "Clients"}
+                  </MDTypography>
+                </MDBox>
+                <MDBox pt={3}>
+                  <DataTable
+                    table={{ columns, rows }}
+                    isSorted={false}
+                    entriesPerPage={false}
+                    showTotalEntries={false}
+                    noEndBorder
+                  />
+                </MDBox>
+              </Card>
+            </Grid>
+          }
           <Grid item xs={12}>
             <Card>
               <MDBox
@@ -71,34 +101,7 @@ function Tables() {
                 coloredShadow="info"
               >
                 <MDTypography variant="h6" color="white">
-                {token?.account === "admin" ? "Commerciaux" : "Clients"}
-                </MDTypography>
-              </MDBox>
-              <MDBox pt={3}>
-                <DataTable
-                  table={{ columns, rows }}
-                  isSorted={false}
-                  entriesPerPage={false}
-                  showTotalEntries={false}
-                  noEndBorder
-                />
-              </MDBox>
-            </Card>
-          </Grid>
-          <Grid item xs={12}>
-            <Card>
-              <MDBox
-                mx={2}
-                mt={-3}
-                py={3}
-                px={2}
-                variant="gradient"
-                bgColor="info"
-                borderRadius="lg"
-                coloredShadow="info"
-              >
-                <MDTypography variant="h6" color="white">
-                 Prospects
+                  Prospects
                 </MDTypography>
               </MDBox>
               <MDBox pt={3}>
